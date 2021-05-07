c5d6be7882a0ec572c36c34aa58db79d8d14f10e
DeweyReed <deweyreed@protonmail.com>
2021-05-07 22:26:32 +0800

    Move Context.safeContext from ContextHelper to SharedPreferenceHelper

--- tools/src/main/java/xyz/aprildown/tools/helper/SharedPreferenceHelper.kt
+++ tools/src/main/java/xyz/aprildown/tools/helper/SharedPreferenceHelper.kt
@@ -5,12 +5,21 @@ package xyz.aprildown.tools.helper
 import android.app.Activity
 import android.content.Context
 import android.content.SharedPreferences
+import android.os.Build
+import androidx.core.content.ContextCompat
 import androidx.core.content.edit
 import androidx.preference.PreferenceManager
 
 /**
  * Created on 2017/11/3.
  */
+
+val Context.safeContext: Context
+    get() {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.N) return this
+        if (!isDeviceProtectedStorage) return this
+        return ContextCompat.createDeviceProtectedStorageContext(this) ?: this
+    }
 
 fun SharedPreferences.getNonNullString(name: String, default: String): String {
     return getString(name, default) ?: default
