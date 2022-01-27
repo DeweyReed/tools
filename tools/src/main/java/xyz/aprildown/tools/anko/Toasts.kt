ae29b19f2556f9a967e747f45f8c3df570b26d67
DeweyReed <32358486+DeweyReed@users.noreply.github.com>
2022-01-27 13:46:23 +0800

    Use ToastCompat

--- tools/src/main/java/xyz/aprildown/tools/anko/Toasts.kt
+++ tools/src/main/java/xyz/aprildown/tools/anko/Toasts.kt
@@ -1,50 +1,75 @@
-@file:Suppress("NOTHING_TO_INLINE", "unused")
+@file:Suppress("unused")
 
 package xyz.aprildown.tools.anko
 
 import android.content.Context
+import android.os.Build
+import android.view.View
 import android.widget.Toast
 import androidx.annotation.StringRes
 import androidx.fragment.app.Fragment
+import me.drakeet.support.toast.ToastCompat
+
+private fun Context.createToast(
+    @StringRes messageRes: Int = 0,
+    message: CharSequence? = null,
+    duration: Int = Toast.LENGTH_SHORT,
+): Toast {
+    return if (Build.VERSION.SDK_INT == Build.VERSION_CODES.N_MR1) {
+        if (messageRes != 0) {
+            ToastCompat.makeText(this, messageRes, duration)
+        } else {
+            ToastCompat.makeText(this, message, duration)
+        }
+    } else {
+        if (messageRes != 0) {
+            Toast.makeText(this, messageRes, duration)
+        } else {
+            Toast.makeText(this, message, duration)
+        }
+    }
+}
 
 // region Short Toast
 
-inline fun Context.toast(@StringRes message: Int): Toast = Toast
-    .makeText(this, message, Toast.LENGTH_SHORT)
-    .apply {
-        show()
-    }
+fun Context.toast(@StringRes message: Int): Toast {
+    return createToast(messageRes = message, duration = Toast.LENGTH_SHORT).apply { show() }
+}
 
-inline fun Context.toast(message: CharSequence): Toast = Toast
-    .makeText(this, message, Toast.LENGTH_SHORT)
-    .apply {
-        show()
-    }
+fun Context.toast(message: CharSequence): Toast {
+    return createToast(message = message, duration = Toast.LENGTH_SHORT).apply { show() }
+}
 
 // endregion
 
 // region Long Toast
 
-inline fun Context.longToast(@StringRes message: Int): Toast = Toast
-    .makeText(this, message, Toast.LENGTH_LONG)
-    .apply {
-        show()
-    }
+fun Context.longToast(@StringRes message: Int): Toast {
+    return createToast(messageRes = message, duration = Toast.LENGTH_LONG).apply { show() }
+}
 
-inline fun Context.longToast(message: CharSequence): Toast = Toast
-    .makeText(this, message, Toast.LENGTH_LONG)
-    .apply {
-        show()
-    }
+fun Context.longToast(message: CharSequence): Toast {
+    return createToast(message = message, duration = Toast.LENGTH_LONG).apply { show() }
+}
 
 // endregion
 
 // region Fragment
 
-inline fun Fragment.toast(@StringRes message: Int): Toast = requireContext().toast(message)
-inline fun Fragment.toast(message: CharSequence): Toast = requireContext().toast(message)
+fun Fragment.toast(@StringRes message: Int): Toast = requireContext().toast(message)
+fun Fragment.toast(message: CharSequence): Toast = requireContext().toast(message)
 
-inline fun Fragment.longToast(@StringRes message: Int): Toast = requireContext().longToast(message)
-inline fun Fragment.longToast(message: CharSequence): Toast = requireContext().longToast(message)
+fun Fragment.longToast(@StringRes message: Int): Toast = requireContext().longToast(message)
+fun Fragment.longToast(message: CharSequence): Toast = requireContext().longToast(message)
+
+// endregion
+
+// region View
+
+fun View.toast(@StringRes message: Int): Toast = context.toast(message)
+fun View.toast(message: CharSequence): Toast = context.toast(message)
+
+fun View.longToast(@StringRes message: Int): Toast = context.longToast(message)
+fun View.longToast(message: CharSequence): Toast = context.longToast(message)
 
 // endregion
