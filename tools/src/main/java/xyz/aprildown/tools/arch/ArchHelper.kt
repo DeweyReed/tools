7d1b1237e33e14391cc12ecd2d3452a75a6a1adb
DeweyReed <32358486+DeweyReed@users.noreply.github.com>
2022-09-24 09:57:34 +0800

    Add Lifecycle.doOnLifecycleEvent

--- tools/src/main/java/xyz/aprildown/tools/arch/ArchHelper.kt
+++ tools/src/main/java/xyz/aprildown/tools/arch/ArchHelper.kt
@@ -3,6 +3,8 @@
 package xyz.aprildown.tools.arch
 
 import androidx.annotation.MainThread
+import androidx.lifecycle.DefaultLifecycleObserver
+import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.Observer
@@ -22,4 +24,51 @@ inline fun <T> LiveData<T?>.observeNonNull(
     }
     observe(owner, wrappedObserver)
     return wrappedObserver
+}
+
+fun Lifecycle.doOnLifecycleEvent(
+    onCreate: (() -> Unit)? = null,
+    onStart: (() -> Unit)? = null,
+    onResume: (() -> Unit)? = null,
+    onPause: (() -> Unit)? = null,
+    onStop: (() -> Unit)? = null,
+    onDestroy: (() -> Unit)? = null,
+) {
+    addObserver(
+        object : DefaultLifecycleObserver {
+            override fun onCreate(owner: LifecycleOwner) {
+                super.onCreate(owner)
+                onCreate?.invoke()
+            }
+
+            override fun onStart(owner: LifecycleOwner) {
+                super.onStart(owner)
+                onStart?.invoke()
+            }
+
+            override fun onResume(owner: LifecycleOwner) {
+                super.onResume(owner)
+                onResume?.invoke()
+            }
+
+            override fun onPause(owner: LifecycleOwner) {
+                super.onPause(owner)
+                onPause?.invoke()
+            }
+
+            override fun onStop(owner: LifecycleOwner) {
+                super.onStop(owner)
+                onStop?.invoke()
+            }
+
+            override fun onDestroy(owner: LifecycleOwner) {
+                super.onDestroy(owner)
+                onDestroy?.invoke()
+            }
+        }
+    )
+}
+
+fun Lifecycle.doOnDestroy(block: () -> Unit) {
+    doOnLifecycleEvent(onDestroy = block)
 }
