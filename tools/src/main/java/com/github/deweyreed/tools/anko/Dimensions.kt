5c80af6ef20a13dd0dabf25b7d2978a1bf832e44
DeweyReed <32358486+DeweyReed@users.noreply.github.com>
2023-05-19 19:41:14 +0800

    Deprecate dip and remove some redundant dp methods

--- tools/src/main/java/com/github/deweyreed/tools/anko/Dimensions.kt
+++ tools/src/main/java/com/github/deweyreed/tools/anko/Dimensions.kt
@@ -1,31 +1,26 @@
-@file:Suppress("NOTHING_TO_INLINE", "unused")
+@file:Suppress("unused")
 
 package com.github.deweyreed.tools.anko
 
 import android.content.Context
-import android.view.View
-import androidx.fragment.app.Fragment
+import android.util.TypedValue
+
+/**
+ * [TypedValue.applyDimension]
+ */
 
 private inline val Context.density: Float get() = resources.displayMetrics.density
 private inline val Context.scaledDensity: Float get() = resources.displayMetrics.scaledDensity
 
 fun Context.dp(value: Int): Float = value * density
+
+@Deprecated(message = "Use dp", replaceWith = ReplaceWith(expression = "dp(value).toInt()"))
 fun Context.dip(value: Int): Int = dp(value).toInt()
+
 fun Context.dp(value: Float): Float = value * density
+
+@Deprecated(message = "Use dp", replaceWith = ReplaceWith(expression = "dp(value).toInt()"))
 fun Context.dip(value: Float): Int = dp(value).toInt()
+
 fun Context.sp(value: Int): Float = value * scaledDensity
 fun Context.sp(value: Float): Float = value * scaledDensity
-
-inline fun View.dp(value: Int): Float = context.dp(value)
-inline fun View.dip(value: Int): Int = context.dip(value)
-inline fun View.dp(value: Float): Float = context.dp(value)
-inline fun View.dip(value: Float): Int = context.dip(value)
-inline fun View.sp(value: Int): Float = context.sp(value)
-inline fun View.sp(value: Float): Float = context.sp(value)
-
-inline fun Fragment.dp(value: Int): Float = requireContext().dp(value)
-inline fun Fragment.dip(value: Int): Int = requireContext().dip(value)
-inline fun Fragment.dp(value: Float): Float = requireContext().dp(value)
-inline fun Fragment.dip(value: Float): Int = requireContext().dip(value)
-inline fun Fragment.sp(value: Int): Float = requireContext().sp(value)
-inline fun Fragment.sp(value: Float): Float = requireContext().sp(value)
