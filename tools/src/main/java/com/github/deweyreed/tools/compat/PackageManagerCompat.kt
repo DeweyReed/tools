b039eafec8cb0f2543c76daf58a74a95b9d6023b
DeweyReed <32358486+DeweyReed@users.noreply.github.com>
2023-05-22 17:39:06 +0800

    Replace PackageManager extension methods with PackageManagerCompat class

--- tools/src/main/java/com/github/deweyreed/tools/compat/PackageManagerCompat.kt
+++ tools/src/main/java/com/github/deweyreed/tools/compat/PackageManagerCompat.kt
@@ -8,31 +8,44 @@ import android.content.pm.PackageManager
 import android.content.pm.ResolveInfo
 import android.os.Build
 
-fun PackageManager.getPackageInfoCompat(packageName: String, flags: Long): PackageInfo {
-    return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-        getPackageInfo(packageName, PackageManager.PackageInfoFlags.of(flags))
-    } else {
-        @Suppress("DEPRECATION")
-        getPackageInfo(packageName, flags.toInt())
+object PackageManagerCompat {
+    fun getPackageInfo(
+        packageManager: PackageManager,
+        packageName: String,
+        flags: Long
+    ): PackageInfo {
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            packageManager.getPackageInfo(packageName, PackageManager.PackageInfoFlags.of(flags))
+        } else {
+            @Suppress("DEPRECATION")
+            packageManager.getPackageInfo(packageName, flags.toInt())
+        }
     }
-}
 
-fun PackageManager.queryIntentActivitiesCompat(intent: Intent, flags: Long): List<ResolveInfo> {
-    return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-        @Suppress("QueryPermissionsNeeded")
-        queryIntentActivities(intent, PackageManager.ResolveInfoFlags.of(flags))
-    } else {
-        @Suppress("DEPRECATION", "QueryPermissionsNeeded")
-        queryIntentActivities(intent, flags.toInt())
+    fun queryIntentActivities(
+        packageManager: PackageManager,
+        intent: Intent,
+        flags: Long
+    ): List<ResolveInfo> {
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            @Suppress("QueryPermissionsNeeded")
+            packageManager.queryIntentActivities(
+                intent,
+                PackageManager.ResolveInfoFlags.of(flags)
+            )
+        } else {
+            @Suppress("DEPRECATION", "QueryPermissionsNeeded")
+            packageManager.queryIntentActivities(intent, flags.toInt())
+        }
     }
-}
 
-fun PackageManager.getInstalledPackages(flags: Long): List<PackageInfo> {
-    return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-        @Suppress("QueryPermissionsNeeded")
-        getInstalledPackages(PackageManager.PackageInfoFlags.of(flags))
-    } else {
-        @Suppress("DEPRECATION", "QueryPermissionsNeeded")
-        getInstalledPackages(flags.toInt())
+    fun getInstalledPackages(packageManager: PackageManager, flags: Long): List<PackageInfo> {
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            @Suppress("QueryPermissionsNeeded")
+            packageManager.getInstalledPackages(PackageManager.PackageInfoFlags.of(flags))
+        } else {
+            @Suppress("DEPRECATION", "QueryPermissionsNeeded")
+            packageManager.getInstalledPackages(flags.toInt())
+        }
     }
 }
