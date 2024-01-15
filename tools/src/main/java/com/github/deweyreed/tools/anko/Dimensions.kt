b69d128fcc370d120278faa1ce4566332d6d0c5c
DeweyReed <32358486+DeweyReed@users.noreply.github.com>
2024-01-15 08:52:39 +0800

    Calculate dp and sp with TypedValue.applyDimension

--- tools/src/main/java/com/github/deweyreed/tools/anko/Dimensions.kt
+++ tools/src/main/java/com/github/deweyreed/tools/anko/Dimensions.kt
@@ -5,15 +5,21 @@ package com.github.deweyreed.tools.anko
 import android.content.Context
 import android.util.TypedValue
 
-/**
- * [TypedValue.applyDimension]
+fun Context.dp(value: Int): Float {
+    return dp(value.toFloat())
+}
+
+fun Context.dp(value: Float): Float {
+    return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, value, resources.displayMetrics)
+}
+
+fun Context.sp(value: Int): Float {
+    return sp(value.toFloat())
+}
+
+/*
+ * [https://developer.android.com/about/versions/14/features#convert-pixels]
  */
-
-private inline val Context.density: Float get() = resources.displayMetrics.density
-private inline val Context.scaledDensity: Float get() = resources.displayMetrics.scaledDensity
-
-fun Context.dp(value: Int): Float = value * density
-fun Context.dp(value: Float): Float = value * density
-
-fun Context.sp(value: Int): Float = value * scaledDensity
-fun Context.sp(value: Float): Float = value * scaledDensity
+fun Context.sp(value: Float): Float {
+    return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP, value, resources.displayMetrics)
+}
